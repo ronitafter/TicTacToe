1f0210f30f665f0568a6f3f724ca311e753ea001
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-04-24 12:13:20 +0300

    stage 2

--- TicTacToe/src/game/TicTacToe.java
+++ TicTacToe/src/game/TicTacToe.java
@@ -1,9 +1,16 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
+	
+	static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+	static ArrayList<Integer> computerPositions = new ArrayList<Integer>();
+
 
 	public static void main(String[] args) {
 		char [][] gameBoard = {
@@ -28,8 +35,9 @@ public class TicTacToe {
 			Random random = new Random();
 			int computerPos= random.nextInt(9)+1;
 			placePiece(gameBoard, computerPos, "computer");
-			
 			pringGameBoard(gameBoard);
+			
+			checkWinner();
 		}
 		
 
@@ -93,6 +101,41 @@ public class TicTacToe {
 	}
 	
 	public static String checkWinner() {
+		List topRow = Arrays.asList(1,2,3);
+		List midRow = Arrays.asList(4,5,6);
+		List botRow = Arrays.asList(7,8,9);
+		
+		List leftCol = Arrays.asList(1,4,7);
+		List midCol = Arrays.asList(2,5,8);
+		List rightCol = Arrays.asList(3,6,9);
+		
+		List cross1 = Arrays.asList(1,5,9);
+		List cross2 = Arrays.asList(7,5,3);
+		
+		
+		List<List> winning = new ArrayList<List>();
+		winning.add(topRow);
+		winning.add(midRow);
+		winning.add(botRow);
+		winning.add(leftCol);
+		winning.add(midCol);
+		winning.add(rightCol);
+		winning.add(cross1);
+		winning.add(cross2);
+		
+		for(List L: winning) {
+			if(playerPositions.containsAll(L)) {
+				return "You Won!";		
+			}else if(computerPositions.containsAll(L)) {
+				return "Computer Won...";			
+			}else if(playerPositions.size() + computerPositions.size()==9){
+				return "it's a tie";
+				
+			}
+			
+		}
+
+
 		return "";
 	}
 
